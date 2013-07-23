ba03f01aa781ae648ed41e9ad3071f2ef492e0e9
sevlyar <sergey.yarmonov@gmail.com>
2013-07-23 21:17:15 +0700

    + sample: реализованы флаги -t -f + sample: реализовано чтение конфигурации из файла + sample: реализована полезная нагрузка - проверка времени модификации указанных файлов

--- sample/dmn.go
+++ sample/dmn.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"go-daemon"
 	"log"
 	"os"
 	"syscall"
+	"time"
 )
 
 const (
@@ -14,44 +16,61 @@ const (
 
 	fileMask = 0600
 )
-
 const (
 	ret_OK = iota
 	ret_ALREADYRUN
 	ret_PIDFERROR
 	ret_REBORNERROR
+	ret_CONFERROR
 )
 
 var (
-	status = flag.Bool("status", false, "check status of the daemon")
-	silent = flag.Bool("silent", false, "don't write in stdout")
+	status = flag.Bool("status", false,
+		`Check status of the daemon. The program immediately exits after these 
+		checks with either a return code of 0 (Daemon Stopped) or return code 
+		not equal to 0 (Daemon Running)`)
+
+	silent = flag.Bool("silent", false, "Don't write in stdout")
+
+	test = flag.Bool("t", false,
+		`Run syntax tests for configuration files only. The program 
+		immediately exits after these syntax parsing tests with either 
+		a return code of 0 (Syntax OK) or return code not equal to 0 
+		(Syntax Error)`)
+
+	configFileName = flag.String("f", "dmn.conf",
+		`Specifies the name of the configuration file. The default is dmn.conf. 
+		Daemon refuses to start if there is no configuration file.`)
 )
+
+var confProv = make(chan Config)
 
 func main() {
 	flag.Parse()
 
 	setupLogging()
 
+	conf, err := loadConfig(*configFileName)
+	if err != nil {
+		log.Println("Config error:", err)
+		os.Exit(ret_CONFERROR)
+	}
+	if *test {
+		os.Exit(ret_OK)
+	}
+
 	pidf := lockPidFile()
 
-	err := daemon.Reborn(027, "./")
+	err = daemon.Reborn(027, "./")
 	if err != nil {
 		log.Println("Reborn error:", err)
 		os.Exit(ret_REBORNERROR)
 	}
 
-	log.Println("--- log ---")
+	confProv <- conf
+	go watchdog(confProv)
 
-	daemon.SetHandler(TermHandler, syscall.SIGTERM, syscall.SIGKILL)
-	daemon.SetHandler(HupHandler, syscall.SIGHUP)
-	daemon.SetHandler(Usr1Handler, syscall.SIGUSR1)
-
-	err = daemon.ServeSignals()
-	if err != nil {
-		log.Println("Error:", err)
-	}
-
-	log.Println("--- end ---")
+	serveSignals()
 
 	pidf.Unlock()
 }
@@ -62,6 +81,7 @@ func setupLogging() {
 		daemon.RedirectStream(os.Stdout, file)
 		daemon.RedirectStream(os.Stderr, file)
 		file.Close()
+		log.Println("--- log ---")
 	} else {
 		log.SetFlags(0)
 		if *silent {
@@ -71,6 +91,28 @@ func setupLogging() {
 			file.Close()
 		}
 	}
+}
+
+type Config []string
+
+func loadConfig(path string) (config Config, err error) {
+	var file *os.File
+	file, err = os.OpenFile(path, os.O_RDONLY, 0700)
+	if err != nil {
+		return
+	}
+	config = make([]string, 0)
+	err = json.NewDecoder(file).Decode(&config)
+	if err != nil {
+		return
+	}
+	for _, path = range config {
+		if _, err = os.Stat(path); os.IsNotExist(err) {
+			return
+		}
+	}
+
+	return
 }
 
 func lockPidFile() *daemon.PidFile {
@@ -85,12 +127,10 @@ func lockPidFile() *daemon.PidFile {
 		}
 	}
 
-	// unlock pid file, if deamon be reborn
 	if !daemon.IsWasReborn() {
 		pidf.Unlock()
 	}
 
-	// normal termination in case status checking
 	if *status {
 		os.Exit(ret_OK)
 	}
@@ -98,17 +138,60 @@ func lockPidFile() *daemon.PidFile {
 	return pidf
 }
 
-func TermHandler(sig os.Signal) error {
+func watchdog(confProv <-chan Config) {
+	states := make(map[string]time.Time)
+	conf := <-confProv
+	for {
+		select {
+		case conf = <-confProv:
+		default:
+		}
+
+		for _, path := range conf {
+			fi, err := os.Stat(path)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+
+			cur := fi.ModTime()
+			if pre, exists := states[path]; exists {
+				if pre != cur {
+					log.Printf("file %s modified at %s", path, cur)
+				}
+			}
+			states[path] = cur
+		}
+		time.Sleep(time.Second)
+	}
+}
+
+func serveSignals() {
+	daemon.SetHandler(termHandler, syscall.SIGTERM, syscall.SIGKILL)
+	daemon.SetHandler(hupHandler, syscall.SIGHUP)
+
+	err := daemon.ServeSignals()
+	if err != nil {
+		log.Println("Error:", err)
+	}
+
+	log.Println("--- end ---")
+}
+
+func termHandler(sig os.Signal) error {
 	log.Println("SIGTERM:", sig)
 	return daemon.ErrStop
 }
 
-func HupHandler(sig os.Signal) error {
+func hupHandler(sig os.Signal) error {
 	log.Println("SIGHUP:", sig)
-	return nil
-}
 
-func Usr1Handler(sig os.Signal) error {
-	log.Println("SIGUSR1:", sig)
-	panic("fatal error")
+	conf, err := loadConfig(*configFileName)
+	if err != nil {
+		log.Println("Config error:", err)
+	} else {
+		confProv <- conf
+	}
+
+	return nil
 }
