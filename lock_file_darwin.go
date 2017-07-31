298c54b0e0ae32ec2c6674fee8b60d2fefa4ae7e
Sergey Yarmonov <sergey.yarmonov@gmail.com>
2017-07-31 23:19:21 +0700

    Remove unused imports of .h files from implementaion for darwin

--- lock_file_darwin.go
+++ lock_file_darwin.go
@@ -2,20 +2,15 @@
 
 package daemon
 
-/*
-#define __DARWIN_UNIX03 0
-#define KERNEL
-#define _DARWIN_USE_64_BIT_INODE
-#include <dirent.h>
-#include <fcntl.h>
-#include <sys/param.h>
-*/
-import "C"
-
 import (
 	"syscall"
 	"unsafe"
 )
+
+import "C"
+
+// darwin's MAXPATHLEN
+const maxpathlen = 1024
 
 func lockFile(fd uintptr) error {
 	err := syscall.Flock(int(fd), syscall.LOCK_EX|syscall.LOCK_NB)
@@ -34,7 +29,7 @@ func unlockFile(fd uintptr) error {
 }
 
 func getFdName(fd uintptr) (name string, err error) {
-	buf := make([]C.char, int(C.MAXPATHLEN)+1)
+	buf := make([]C.char, maxpathlen+1)
 	_, _, errno := syscall.Syscall(syscall.SYS_FCNTL, fd, syscall.F_GETPATH, uintptr(unsafe.Pointer(&buf[0])))
 	if errno == 0 {
 		return C.GoString(&buf[0]), nil
